19c06c4298a37d375aed0974203ffcaf627708ef
aloe-99 <100123642+aloe-99@users.noreply.github.com>
2023-06-09 14:57:44 +0300

    correct links

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -11,10 +11,30 @@ function MyProjects() {
     <section className='projects section' id='projects'>
       <h2 className='section__title'>Проекты</h2>
       <ul className='projects__container'>
-        <ProjectCard imagePath={howLearnImage} projectName='How-to-learn' projectLink='https://github.com/aloe-99/how-to-learn' />
-        <ProjectCard imagePath={russianTravelImage} projectName='Russian-travel' projectLink='https://aloe-99.github.io/russian-travel/' />
-        <ProjectCard imagePath={mestoImage} projectName='Mesto' projectLink='https://github.com/aloe-99/react-mesto-api-full' />
-        <ProjectCard imagePath={moviesImage} projectName='Movies-explorer' projectLink='https://github.com/aloe-99/movies-explorer-frontend' />
+        <ProjectCard
+          imagePath={howLearnImage}
+          projectName='How-to-learn'
+          projectLink='https://github.com/aloe-99/how-to-learn'
+          projectDomain="https://aloe-99.github.io/how-to-learn/"
+        />
+        <ProjectCard
+          imagePath={russianTravelImage}
+          projectName='Russian-travel'
+          projectLink='https://github.com/aloe-99/russian-travel'
+          projectDomain="https://aloe-99.github.io/russian-travel/"
+        />
+        <ProjectCard
+          imagePath={mestoImage}
+          projectName='Mesto'
+          projectLink='https://github.com/aloe-99/react-mesto-api-full'
+          projectDomain="mesto.aloe.nomoredomains.icu"
+        />
+        <ProjectCard
+          imagePath={moviesImage}
+          projectName='Movies-explorer'
+          projectLink='https://github.com/aloe-99/movies-explorer-frontend'
+          projectDomain="movies.aloe.nomoredomains.club"
+        />
       </ul>
     </section>
   );
--- src/components/MyProjects/ProjectCard/ProjectCard.js
+++ src/components/MyProjects/ProjectCard/ProjectCard.js
@@ -1,14 +1,18 @@
 import './ProjectCard.css';
 
 function ProjectCard(props) {
-  const { imagePath, projectName, projectLink } = props;
+  const { imagePath, projectName, projectLink, projectDomain } = props;
+
+  function handleOpenProject() {
+    window.open(projectDomain);
+  }
 
   return (
     <li className='projects__card'>
-      <img className='projects__image' src={imagePath} alt='Фото проекта' />
+      <img className='projects__image' src={imagePath} onClick={handleOpenProject} alt='Фото проекта' />
       <div className='projects__about'>
         <p className='projects__name'>{projectName}</p>
-        <a className='projects__about-link link-dissolution' href={projectLink}>Репозиторий</a>
+        <a className='projects__about-link link-dissolution' href={projectLink} target='_blank'>Репозиторий</a>
       </div>
     </li>
   );
